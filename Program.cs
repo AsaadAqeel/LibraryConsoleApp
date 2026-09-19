5b6ff3cf9ed4ebcddcd512d89e557a623842ce13
AsaadAqeel <asd.akil444@gmail.com>
2026-09-19 12:39:43 +0300

    ConsoleAPP1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleapp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName;
+            string lastName;
+            string fullName;
+
+            Console.WriteLine("Hello to my first console app!");
+
+            Console.WriteLine("Enter your first name:");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name");
+            lastName = Console.ReadLine();
+
+            fullName = firstName + " " + lastName;
+            Console.WriteLine("Welcome {0} to my first console app!", fullName);
+        }
+    }
+}
